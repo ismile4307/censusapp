ea9461f5a7ee7f31ee478cf46f69e663f8c015d2
Habibur81 <mdhabiburr375@gmail.com>
2023-05-14 15:23:38 +0600

    RouteChange 05.14.2023

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ use App\Http\Controllers\FilterParametersController;
 use App\Http\Controllers\ShowingVariablesController;
 use App\Http\Controllers\SupportInfoController;
 use App\Http\Controllers\Settings\ProjectUsersController;
-use App\Http\Controllers\RespondentpanelController;
 
 /*
 |--------------------------------------------------------------------------
